ef674a213f166c68fc909b2d59671a8cab67499e
dale.marion.j.perez <dale.marion.j.perez>
2018-12-10 18:23:26 +0800

    New MicroServices

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into user values(10001, sysdate(), 'AB');
+insert into user values(10002, sysdate(), 'BB');
+insert into user values(10003, sysdate(), 'BC');
+insert into post values(11001, 'My First Post', 10001);
+insert into post values(11002, 'My First Post', 10002);
